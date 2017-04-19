efc7a7e99d899c18fa274f1a3e9b3617f1fb25b2
sevtdy <sevtdy@gmail.com>
2017-04-19 20:45:40 +0800

    增加errorpage

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import datepicker from './components/datepicker.vue'
 import hello from './components/hello.vue'
 import pickeddate from './components/pickeddate.vue'
+import errorpage from './components/errorpage.vue'
 
 export default [
   { path: '/hello', component: hello },
   { path: '/', component: datepicker },
+  { path: '/error', component: errorpage },
   { path: '*', component: pickeddate }
 ]
